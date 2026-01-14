eefce667b41c6850ea34fdf3d5897a029f672031
nachool <nachma.olivia1394@gmail.com>
2026-01-14 13:25:45 +0700

    first commit

---
+++ config/config.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const development = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
+};
+
+const test = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE_TEST,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
+};
+
+const production = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE_PROD,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
+};
+
+module.exports = { development, test, production };
---
+++ models/music.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const Music = sequelize.define("Music", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nama: {
+      type: DataTypes.STRING,
+    },
+    author: {
+      type: DataTypes.STRING,
+    },
+    tahun_terbit: {
+      type: DataTypes.INTEGER,
+    },
+    genre: {
+      type: DataTypes.STRING,
+    },
+  });
+  return Music;
+};
